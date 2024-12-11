2f118358785ba1f67846f78f2676c49080c3d1e2
Badnam019 <yadavxd746@gmail.com>
2024-12-11 15:47:54 +0530

    Update repo.py

--- SONALI/plugins/bot/repo.py
+++ SONALI/plugins/bot/repo.py
@@ -43,7 +43,7 @@ InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"@system_music_prorobot"),
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://files.catbox.moe/4z6upe.jpg",
+        photo="https://files.catbox.moe/9nq6hd.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
