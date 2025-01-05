914689e4307750021bfa50733cbe73afd54c6f0b
Badnam xd <yadavxd746@gmail.com>
2025-01-05 08:33:06 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -91,10 +91,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
 def stream_markup(_, videoid, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="ѕкιρ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],
         [
