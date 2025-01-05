51001da5e8eda4ebc3599ebb0f2173fb4f56b262
Vampire674 <vampirelegend5001@gmail.com>
2025-01-05 19:57:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","lll_BADNAM_BABY_lll")
+OWNER_USERNAME = getenv("OWNER_USERNAME","llKILLER_OPll")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","system_music_prorobot")
+BOT_USERNAME = getenv("BOT_USERNAME","Shayarmusic_bot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
@@ -44,15 +44,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badnam019/System_music",
+    "https://github.com/Vampire674/GAANAMUSIC2.0",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/friends_kiDuniya")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT","https://t.me/SYSTEM_SUPPROT_GROUP")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KILLER_UPDATE")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT","https://t.me/KILLER_UPDATE")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
